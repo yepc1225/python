5b88d038cdf92b3dbebdd95dc186ae0eaee8cf1e
yepc1225 <yepc1225@thundersoft.com>
2018-02-28 15:59:40 +0800

    first

---
+++ mh.py
@@ -0,0 +1,13 @@
+num=10
+people=[]
+for i in range(num):
+    people.append(i)
+print(people)
+
+def tt(people):
+    while len(people)==1:
+        for x in people:
+            if (x+1)%3==0:
+                del people[x+1]
+tt(people)
+print(people)
